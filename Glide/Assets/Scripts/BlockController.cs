cef44aa0528badf2633226ccc709506e04b422b5
deepmicrobe <leanderstevano@gmail.com>
2018-04-23 17:31:36 -0400

    updated classic mode

---
+++ Glide/Assets/Scripts/BlockController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    /*  Detect if object was clicked
+     *  Match object transform to mouse transform
+     */
+    private Vector3 distance;
+    Transform objectPosition;
+    Vector3 originalPosition;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    public void OnMouseDown()
+    {
+        // Distance from camera to object
+        distance = Camera.main.WorldToScreenPoint(transform.position);
+    }
+
+    public void OnMouseDrag()
+    {
+        // Get position of mouse while moving it
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance.z);
+        Vector3 objectPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        objectPosition.x = (int)(Mathf.Round(objectPosition.x));
+        objectPosition.y = (int)(Mathf.Round(objectPosition.y));
+        transform.position = objectPosition;
+    }
+
+    public void OnMouseExit()
+    {
+        if(transform.position.x < 0 || transform.position.x > 10 || transform.position.y < 0 || transform.position.y > 10)
+        {
+            transform.position = originalPosition;
+        }
+    }
+}
--- Glide/Assets/Scripts/Grid.cs
+++ Glide/Assets/Scripts/Grid.cs
@@ -20,14 +20,7 @@ public class Grid : MonoBehaviour {
         {
             for(int j = 0; j < 10; j++)
             {
-                //Instantiate(block, new Vector3(j + block.transform.position.x, i + block.transform.position.y, 0), Quaternion.identity);
-                //transform.localScale = new Vector3(0.9F, 0.9F, 0);
-                //Transform clone;
                 Instantiate(block, new Vector3(j + block.transform.position.x, i + block.transform.position.y, 0), Quaternion.identity);
-                //transform.localScale = new Vector3(0.5F, 0.5F, 0);
-                //clone = Instantiate(block, new Vector3(j, i, 0), Quaternion.identity) as Transform;
-                //clone.transform.localScale -= new Vector3(0.5F, 0.5F, 0);
-                //clone.name = "Board Space ( x = " + x.ToString() + " , y =" + y.ToString() + " )";
             }
         }
     }
